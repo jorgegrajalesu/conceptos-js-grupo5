14d97e832ea0963ac9b475eaa2de1d0174ad1bde
jorgegrajalesu <jegosystem@gmail.com>
2023-07-12 21:14:25 -0500

    120723

---
+++ js/variables.js
@@ -0,0 +1,18 @@
+/**
+ * para declarar las variables var, let y const
+ * tipos datos: null, undefined, string(texto), boolean(true, false), number, symbol y object
+ */
+
+let miEdad=35;//tipo numero
+
+const miNombre = 'Jorge'; // tipo texto
+
+const casado = false; // tipo booleano
+
+// miNombre = 'Fausto';
+
+miEdad = 50;
+
+console.log('Mi nombre: ' + miNombre);
+console.log('Mi edad: ' + miEdad);
+console.log('Estado Civil: ' + casado);
