489b17a0994bf0a974b4fa3d743d7b4e138f1fb4
jorgegrajalesu <jegosystem@gmail.com>
2023-07-14 18:22:23 -0500

    140723

---
+++ js/arreglos.js
@@ -0,0 +1,10 @@
+// los arreglos sirven para contener varias variables
+// sintaxis se declara como una variable let o const 
+
+const auto = [];// array o arreglo vacio 
+let  Auto = ['Deportivo', 'rojo', 'automatico', 250000000];
+
+// mostrar los datos del arreglo
+console.log(auto);
+console.log({Auto});
+
