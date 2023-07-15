594201780eff6a463cd7fcd8abaa44f7dd3503b3
jorgegrajalesu <jegosystem@gmail.com>
2023-07-14 20:56:58 -0500

    funciones140723

--- js/funciones.js
+++ js/funciones.js
@@ -4,5 +4,10 @@
 // funcion tradicional sin argumentos
 function miNombre(){
     console.log('Mi nombre es: ' + 'Jorge');   
+    console.log(`Mi nombre es: ${'Marta'}`);   
+    console.log(`Mi nombre es: ${'Julio'}`);   
 
 }
+
+// llamar la función
+miNombre();
