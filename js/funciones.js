854844f95dc96bd943674e8e7b306a3c73a904af
jorgegrajalesu <jegosystem@gmail.com>
2023-07-14 20:50:38 -0500

    140723

---
+++ js/funciones.js
@@ -0,0 +1,8 @@
+/**
+ * Hay varios tipos de funciones: tradicional, de flecha, anonima y asincrona
+ */
+// funcion tradicional sin argumentos
+function miNombre(){
+    console.log('Mi nombre es: ' + 'Jorge');   
+
+}
