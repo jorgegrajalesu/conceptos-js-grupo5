b69cc9323fc665c29a8b472755400e9a03912362
jorgegrajalesu <jegosystem@gmail.com>
2023-07-19 20:02:16 -0500

    190723

---
+++ js/condicionales.js
@@ -0,0 +1,12 @@
+// if y else, determina si la condición se cumple o no
+// ejemplo1: Mostrar el nombre del profe de frontend 
+
+let nombreMataria = 'frontend';
+
+if (nombreMataria==='frontend') {
+    // mostrar nombre del profe
+    console.log(`Nombre del profe: Jorge - ${nombreMataria}`);
+    
+}else{
+    console.log('No es el nombre del profe');
+}
