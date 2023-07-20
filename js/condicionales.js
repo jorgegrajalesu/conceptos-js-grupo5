af8a69f73c32953d255b18549911e2acfec5c042
jorgegrajalesu <jegosystem@gmail.com>
2023-07-19 20:17:27 -0500

    190723

--- js/condicionales.js
+++ js/condicionales.js
@@ -10,3 +10,18 @@ if (nombreMataria==='frontend') {
 }else{
     console.log('No es el nombre del profe');
 }
+
+/**
+ *  ejemplo2: Determinar el ingreso a un bar, solo pueden ingresar los mayores de edad.
+ * */ 
+console.warn('ejemplo2: Determinar el ingreso');
+
+let edadPersona = 18;
+
+if (edadPersona >= 18) {
+
+    console.log(`Tu edad es: ${edadPersona} - puedes ingresar`);
+    
+}else{
+    console.log(`Tu edad es: ${edadPersona} - no puedes ingresar`);
+}
